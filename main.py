2b2fe8dbaa171a51b0d2d97b88e6f6a80edb5386
UsmanGhani <mr.usman1993@gmail.com>
2026-03-05 11:18:28 +0500

    return response (error) :sparkles:

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from langchain-chatbot2!")
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOllama(
+    model ="glm-5:cloud",
+    temprature=0.7
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful AI assistant"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("human", "{question}")
+    ])
+
+chain = prompt | llm | StrOutputParser()
+
+chat_history = []
+
+def chat(question):
+    response = chain.invoke(
+        {
+            "question": question,
+            "chat_history": chat_history
+        }
+    )
+    
+    chat_history.append(AIMessage(content="response"))
+    chat_history.append(HumanMessage(content=question))
+    
+    return(response)
 
 
-if __name__ == "__main__":
-    main()
+print(chat("What is Rag?"))
+print(chat("give me a Python example of it"))
+print(chat("Now explain the code you just gave"))
